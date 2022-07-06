05b94de08b1a690badf26f589a7cb127c23f0b8a
Luis Felipe Echeverry <luis.echeverry@roiback.com>
2022-07-06 17:23:43 -0500

    inicio curso, clase 38

---
+++ src/FirstApp.jsx
@@ -0,0 +1,18 @@
+
+
+export const FirstApp = () => {
+  return (
+    <h1>Hi sekai</h1>
+  )
+}
+
+export const SecodnApp = () =>{
+
+    const sum = (a, b) => {
+      return a + b
+    }
+
+    return (
+        <h2>{ sum(5,7) }</h2>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { FirstApp, SecodnApp } from './FirstApp';
+
+
+ReactDOM.createRoot( document.getElementById('root') ).render(
+    <React.StrictMode>
+        <SecodnApp />
+    </React.StrictMode>
+)
